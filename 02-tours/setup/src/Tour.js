59300636112076b374c3d33fe6cf0e6831cf88ad
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-12 00:44:06 -0500

    finished

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Tour = ({ id, image, price, info, name }) => {
+const Tour = ({ removeTour, id, image, price, info, name }) => {
 	const [readMore, setReadMore] = useState(false);
 	return (
 		<article className="single-tour">
@@ -19,7 +19,9 @@ const Tour = ({ id, image, price, info, name }) => {
 					</button>
 				</p>
 
-				<button className="delete-btn">not interested</button>
+				<button className="delete-btn" onClick={() => removeTour(id)}>
+					not interested
+				</button>
 			</footer>
 		</article>
 	);
