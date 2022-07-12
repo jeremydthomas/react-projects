3c744204a5c7a31c17a2cce1e43cb018cd953a29
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-11 20:24:14 -0500

    added tours

--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, price, info, name }) => {
+	return (
+		<article className="single-tour">
+			<img src={image} alt={name}></img>
+			<footer>
+				<div className="tour-info">
+					<h4>{name}</h4>
+					<h4 className="tour-price">
+						<span>${price}</span>
+					</h4>
+				</div>
+				<p>
+					{info}
+					<button>read more</button>
+				</p>
+				<button className="delete-btn">not interested</button>
+			</footer>
+		</article>
+	);
 };
 
 export default Tour;
