e4e61bf5ea765e307fc771a774da7eda14d70341
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-13 14:26:14 -0500

    finished project

--- 04-accordion/setup/src/Question.js
+++ 04-accordion/setup/src/Question.js
@@ -1,7 +1,20 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { BiPlus, BiMinus } from "react-icons/bi";
+
+const Question = (question) => {
+	const [open, setOpen] = useState(false);
+	const { title, info } = question;
+	return (
+		<article className="question">
+			<header>
+				<h4>{title}</h4>
+				<button className="btn" onClick={() => setOpen(!open)}>
+					{open ? <BiMinus /> : <BiPlus />}
+				</button>
+			</header>
+			{open ? <p>{info}</p> : ""}
+		</article>
+	);
 };
 
 export default Question;
