cc4ca420dadc6bbd699b49672cfa483e7a3c8b14
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-13 19:39:01 -0500

    finished

--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,102 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, allCategories }) => {
+	return (
+		<div className="btn-container">
+			{allCategories.map((category, index) => {
+				return (
+					<button
+						type="button"
+						className="filter-btn"
+						key={index}
+						onClick={() => filterItems(category)}
+					>
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
 };
+// <div className="btn-container">
+// 	<button
+// 		type="button"
+// 		className="filter-btn"
+// 		onClick={() => {
+// 			filterItems("all");
+// 		}}
+// 	>
+// 		all
+// 	</button>
+// 	<button
+// 		type="button"
+// 		className="filter-btn"
+// 		onClick={() => {
+// 			filterItems("breakfast");
+// 		}}
+// 	>
+// 		breakfast
+// 	</button>
+// 	<button
+// 		type="button"
+// 		className="filter-btn"
+// 		onClick={() => {
+// 			filterItems("lunch");
+// 		}}
+// 	>
+// 		lunch
+// 	</button>
+// 	<button
+// 		type="button"
+// 		className="filter-btn"
+// 		onClick={() => {
+// 			filterItems("shakes");
+// 		}}
+// 	>
+// 		shakes
+// 	</button>
+// </div>
+// );
+// return (
+// 	<div className="btn-container">
+// 		<button
+// 			type="button"
+// 			className="filter-btn"
+// 			onClick={() => {
+// 				filterItems("all");
+// 			}}
+// 		>
+// 			all
+// 		</button>
+// 		<button
+// 			type="button"
+// 			className="filter-btn"
+// 			onClick={() => {
+// 				filterItems("breakfast");
+// 			}}
+// 		>
+// 			breakfast
+// 		</button>
+// 		<button
+// 			type="button"
+// 			className="filter-btn"
+// 			onClick={() => {
+// 				filterItems("lunch");
+// 			}}
+// 		>
+// 			lunch
+// 		</button>
+// 		<button
+// 			type="button"
+// 			className="filter-btn"
+// 			onClick={() => {
+// 				filterItems("shakes");
+// 			}}
+// 		>
+// 			shakes
+// 		</button>
+// 	</div>
+// );
+// };
 
 export default Categories;
--- 05-menu/setup/src/Menu.js
+++ 05-menu/setup/src/Menu.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({ menuItems }) => {
+	return (
+		<div className="section-center">
+			{menuItems.map((item) => {
+				const { id, title, price, img, desc } = item;
+				return (
+					<article key={id} className="menu-item">
+						<img className="photo" src={img} alt={title} />
+						<div className="item-info">
+							<header>
+								<h4>{title}</h4>
+								<h4 className="price">${price}</h4>
+							</header>
+							<p className="item-text">{desc}</p>
+						</div>
+					</article>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Menu;
