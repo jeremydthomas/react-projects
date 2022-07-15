b726f4bc8f3d62bfaf36faa96b6ebf9c39be2743
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-15 17:29:13 -0500

    mooo

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import Review from "./Review";
+import data from "./data";
+import Reviews from "./Reviews";
+
 function App() {
-  return <h2>slider project setup</h2>;
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = useState(0);
+	// const prev = () => {
+	// 	setIndex((index) => (index === 0 ? people.length - 1 : index - 1));
+	// };
+
+	// const next = () => {
+	// 	setIndex((index) => (index === people.length - 1 ? 0 : index + 1));
+	// };
+
+	useEffect(() => {
+		const lastIndex = people.length - 1;
+		if (index < 0) {
+			setIndex(lastIndex);
+		}
+		if (index > lastIndex) {
+			setIndex(0);
+		}
+	}, [index, people]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIndex((index) => (index === people.length - 1 ? 0 : index + 1));
+		}, 3000);
+		return () => clearInterval(interval);
+	}, [index, people]);
+
+	return (
+		<section className="section">
+			<Reviews />
+			<Review people={people} index={index} setIndex={setIndex} />
+		</section>
+	);
 }
 
 export default App;
---
+++ 07-slider/setup/src/Reviews.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Reviews() {
+  return <div className="title">
+	<h2>
+		<span>/</span>reviews
+	</h2>
+</div>;
+}
+
