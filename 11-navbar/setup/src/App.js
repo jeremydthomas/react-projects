b60005b0ee3dd5bfff7337539e56e0307c3a7eb6
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-25 02:03:51 -0500

    initial

--- 11-navbar/setup/src/App.js
+++ 11-navbar/setup/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
 function App() {
-  return (
-    <>
-      <h2>navbar project setup</h2>
-    </>
-  )
+	return (
+		<>
+			<nav>
+				<Navbar />
+			</nav>
+		</>
+	);
 }
 
-export default App
+export default App;
