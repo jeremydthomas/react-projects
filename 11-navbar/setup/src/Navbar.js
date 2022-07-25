ee264d94bc4de03684e595c692c5adfe20649bf5
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-25 02:00:30 -0500

    hi

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,48 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+	const navRef = useRef(null);
 
-export default Navbar
+	const handleClick = () => {
+		navRef.current.style.height
+			? (navRef.current.style.height = "")
+			: (navRef.current.style.height = "200px");
+		console.log(navRef.current);
+	};
+
+	return (
+		<div className="nav-center">
+			<div className="nav-header">
+				<img src={logo} alt="logo" className="logo" />
+				<button onClick={handleClick} className="nav-toggle">
+					<FaBars />
+				</button>
+			</div>
+			<div className="links-container" ref={navRef}>
+				<ul className="links">
+					{links.map((link) => {
+						return (
+							<li key={link.id}>
+								<a href={link.url}>{link.text}</a>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div className="social-icons">
+				{social.map((icon) => {
+					return (
+						<a key={icon.id} href={icon.url}>
+							{icon.icon}
+						</a>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
