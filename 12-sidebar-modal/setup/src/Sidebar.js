def25921d65bd91cbb5f09b5ef0c16b2b599d45c
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-07-27 02:12:37 -0500

    finish

--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+	const { isSidebarOpen, closeSidebar } = useGlobalContext();
+	return (
+		<aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+			<div className="sidebar-header">
+				<img src={logo} className="logo" alt="logo" />
+				<button onClick={closeSidebar} className="close-btn">
+					<FaTimes />
+				</button>
+			</div>
+			<ul className="links">
+				{links.map(({ id, url, text, icon }) => (
+					<li key={id}>
+						<a href={url}>
+							{icon}
+							{text}
+						</a>
+					</li>
+				))}
+			</ul>
+			<ul className="social-icons">
+				{social.map(({ id, url, icon }) => (
+					<li key={id}>
+						<a href={url}>{icon}</a>
+					</li>
+				))}
+			</ul>
+		</aside>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
