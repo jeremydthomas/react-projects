137f24f15a635c07535745b64d97045fe205f36e
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-08-24 18:02:46 -0500

    initial

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,4 +1,56 @@
 const reducer = (state, action) => {
+	if (action.type === "CLEAR_CART") {
+		return { ...state, cart: [] };
+	}
+
+	if (action.type === "REMOVE") {
+		return {
+			...state,
+			cart: state.cart.filter((cartItem) => cartItem.id !== action.payload),
+		};
+	}
+
+	if (action.type === "INCREASE") {
+		let tempCart = state.cart.map((cartItem) => {
+			if (cartItem.id === action.payload) {
+				return { ...cartItem, amount: cartItem.amount + 1 };
+			}
+			return cartItem;
+		});
+		return {
+			...state,
+			cart: tempCart,
+		};
+	}
+
+	if (action.type === "DECREASE") {
+		let tempCart = state.cart.map((cartItem) => {
+			if (cartItem.id === action.payload && cartItem.amount > 0) {
+				return { ...cartItem, amount: cartItem.amount - 1 };
+			}
+			return cartItem;
+		});
+		return {
+			...state,
+			cart: tempCart,
+		};
+	}
+
+	// if (action.type === "DECREASE") {
+	// 	let tempCart = state.cart.map((cartItem) => {
+	// 		if (cartItem.id === action.payload) {
+	// 			if (cartItem.amount > 0) {
+	// 				return { ...cartItem, amount: cartItem.amount - 1 };
+	// 			}
+	// 		}
+	// 		return cartItem;
+	// 	});
+	// 	return {
+	// 		...state,
+	// 		cart: tempCart,
+	// 	};
+	// }
+
 	return state;
 };
 
