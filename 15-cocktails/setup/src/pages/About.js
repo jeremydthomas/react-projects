bbdad0c4e9772d07400c45c091888edf72e2d85f
jeremydthomas <tommyhilfiger12@hotmail.com>
2022-08-29 02:15:46 -0500

    navbar about and error pages

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta cum
+				recusandae animi earum distinctio quasi, ea consequuntur sit ipsam ipsum
+				similique delectus aspernatur est iusto molestiae error omnis sint
+				quidem fuga quo quas, doloremque at. Consectetur, culpa cumque aliquam
+				rem id voluptate totam maxime consequatur dolore quisquam quas
+				voluptatum exercitationem?
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
--- 15-cocktails/setup/src/pages/Home.js
+++ 15-cocktails/setup/src/pages/Home.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import CocktailList from '../components/CocktailList'
-import SearchForm from '../components/SearchForm'
+import React from "react";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
 
 const Home = () => {
-  return (
-    <div>
-      <h2>home page</h2>
-    </div>
-  )
-}
+	return (
+		<main>
+			<section className="section search">
+				<SearchForm />
+				<CocktailList />
+			</section>
+		</main>
+	);
+};
 
-export default Home
+export default Home;
