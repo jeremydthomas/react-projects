4bf772178b7832e4530464d5fa1176c4ab404266
Jeremy Thomas <tommyhilfiger12@hotmail.com>
2022-10-13 21:10:01 -0500

    finished markdown

--- 16-markdown-preview/setup/src/App.js
+++ 16-markdown-preview/setup/src/App.js
@@ -1,8 +1,27 @@
-import React, { useState } from 'react'
-import ReactMarkdown from 'react-markdown'
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>markdown preview starter</h2>
+	const [markdown, setMarkdown] = useState("## markdown preview");
+
+	function handleChange(e) {
+		setMarkdown(e.target.value);
+	}
+	return (
+		<section className="markdown">
+			<textarea
+				type="text"
+				value={markdown}
+				className="input"
+				onChange={(e) => handleChange(e)}
+			></textarea>
+			<article className="result">
+				<h1>
+					<ReactMarkdown children={markdown} />
+				</h1>
+			</article>
+		</section>
+	);
 }
 
-export default App
+export default App;
