5f3b91855f58461c6e8d097052f4302773a92e37
Jeremy Thomas <tommyhilfiger12@hotmail.com>
2022-10-22 03:50:43 -0500

    hi

--- 19-stock-photos/setup/src/App.js
+++ 19-stock-photos/setup/src/App.js
@@ -1,12 +1,74 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import Photo from "./Photo";
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+	const [loading, setLoading] = useState(true);
+	const [pics, setPics] = useState([]);
+	const [value, setValue] = useState("");
+
+	const getPics = async () => {
+		let url;
+		url = `${mainUrl}${clientID}`;
+		const response = await fetch(url);
+		const data = await response.json();
+		console.log(data);
+		setPics(data);
+		setLoading(false);
+	};
+
+	useEffect(() => {
+		getPics();
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		const searchPics = async () => {
+			let url;
+			url = `${searchUrl}${clientID}?page=2&query=${value}`;
+			console.log(url);
+			const response = await fetch(url);
+			const data = await response.json();
+			console.log(data.results, "search");
+			const { results } = data;
+			setPics(results);
+			setLoading(false);
+		};
+		searchPics();
+	};
+
+	return (
+		<main>
+			<section className="search">
+				<form className="search-form">
+					<input
+						type="text"
+						value={value}
+						className="form-input"
+						placeholder="search"
+						onChange={(e) => setValue(e.target.value)}
+					/>
+					<button type="submit" className="submit-btn" onClick={handleSubmit}>
+						<FaSearch />
+					</button>
+				</form>
+			</section>
+
+			<section className="photos">
+				<div className="photos-center">
+					{pics.map((pic) => {
+						console.log(pic);
+						return <Photo key={pic.id} {...pic} />;
+					})}
+				</div>
+				{loading && <h2 className="loading"> Loading....</h2>}
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
--- 19-stock-photos/setup/src/Photo.js
+++ 19-stock-photos/setup/src/Photo.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({ urls, user, likes, alt_description }) => {
+	return (
+		<article className="photo">
+			<img src={urls.regular} alt={alt_description} />
+			<div className="photo-info">
+				<div>
+					<h4>{user.name}</h4>
+					<p>{likes}</p>
+				</div>
+				<a href={user.portfolio_url}>
+					<img
+						className="user-img"
+						src={user.profile_image.medium}
+						alt={user.name}
+					/>
+				</a>
+			</div>
+		</article>
+	);
+};
 
-export default Photo
+export default Photo;
